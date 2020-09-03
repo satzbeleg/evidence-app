3663f15eabb3526b32038dba85dc9aae5c5058ff
Ulf Hamster <554c46@gmail.com>
2020-09-03 18:02:49 +0200

    fix #8 hello world removed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,41 @@
   <section class="section" id="home">
     <div class="container">
       <!-- put the following into components ... -->
-      <img alt="Vue logo" src="../assets/logo.png">
-      <HelloWorld msg="Howto examples"/>
+      <div class="content">
+        <h1 class="title">{{ $t('home.welcome') }}</h1>
+        <p class="subtitle">{{ $t('home.welcome_text') }}</p>
+      </div>
+
+      <div class="card">
+        <div class="card-content">
+          <h1 class="title is-4">
+            Best-Worst Scaling
+          </h1>
+          <div class="content">
+            Es werden 4 Satzbeispiele angezeigt. 
+            Markiere zuerst das <strong>beste</strong> Satzbeispiel an.
+            Zum Schluß wähle noch das <strong>schlechteste</strong> Satzbeispiel aus.
+          </div>
+          <router-link :to="{ path: '/best-worst-scaling' }">
+            <a class="button is-primary">Start</a>
+          </router-link>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-content">
+          <h1 class="title is-4">
+            Binary Labelling
+          </h1>
+          <div class="content">
+            Label 1 Satzbeispiel als <strong>gut</strong> (swipe right)
+            oder <strong>schlecht</strong> (swipe left).
+          </div>
+          <router-link :to="{ path: '/binary-labelling' }">
+            <a class="button is-primary">Start</a>
+          </router-link>
+        </div>
+      </div>
       <!-- put the above into components ... -->
     </div>
   </section>
@@ -11,16 +44,7 @@
 
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  metaInfo: {
-    title: 'Hello World!'
-  },
-  components: {
-    HelloWorld
-  }
 }
 </script>
