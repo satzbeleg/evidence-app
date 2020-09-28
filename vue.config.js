22312a2e3923ea0cf4d327efe030ab706d2f124c
Ulf Hamster <554c46@gmail.com>
2020-09-28 11:19:32 +0200

    try a snippet from the vue forum

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,38 @@
-// vue.config.js
+// https://forum.vuejs.org/t/how-to-preload-css-in-vue/90710/2
+const webpack = require("webpack");
+
 module.exports = {
+  configureWebpack: {
+    plugins: [new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)],
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        chunks: "all",
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        maxSize: 200000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return `npm.${packageName.replace("@", "")}`;
+            }
+          }
+        }
+      }
+    }
+  },
   chainWebpack: config => {
-    config.plugin('prefetch')
-    config.plugin('preload')
+    config.module
+      .rule("i18n")
+      .resourceQuery(/blockType=i18n/)
+      .type("javascript/auto")
+      .use("i18n")
+      .loader("@kazupon/vue-i18n-loader")
+      .end();
+    config.performance.maxEntrypointSize(400000).maxAssetSize(400000);
   }
-}
+};
