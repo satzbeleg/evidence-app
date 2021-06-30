738df910c8a4a2e5947b987ef5709bdc210714b8
UH <554c46@gmail.com>
2021-06-30 13:52:20 +0200

    other pages for legal stuff

---
+++ src/views/legal/Consent.vue
@@ -0,0 +1,23 @@
+<template>
+  <TheNavbar v-bind:with_lang_switch="true"
+             v-bind:with_darkmode_icon="true"
+             v-bind:with_lemmata_search="false" />
+  <section class="section" id="consent">
+    <div class="container">
+      <!-- put the following into components ... -->
+      
+      <h1 class="title is-3">{{ t('consent.title') }}</h1>
+      <p>
+      In er EVIDENCE App werden Sie gefragt, Satzbelege zu einem gegebenen Lemma zu bewerten. Um die Daten zu sammeln und weiter auswerten zu können, benötigen wir Ihre Einwilligung. Im Folgenden klären wir Sie darüber auf, welche Informationen zu welchem Zweck in dieser Studie gesammelt werden.
+      </p>
+
+      <h2 class="subtitle is-5">Lorem </h2>
+      <div class="content">
+        <p>ipsum</p>
+      </div>
+
+      <!-- put the above into components ... -->
+    </div>
+  </section>
+</template>
+
---
+++ src/views/legal/Terms.vue
@@ -0,0 +1,20 @@
+<template>
+  <TheNavbar v-bind:with_lang_switch="true"
+             v-bind:with_darkmode_icon="true"
+             v-bind:with_lemmata_search="false" />
+  <section class="section" id="terms">
+    <div class="container">
+      <!-- put the following into components ... -->
+      
+      <h1 class="title is-3">{{ t('terms.title') }}</h1>
+      <p></p>
+
+      <h2 class="subtitle is-5">Lorem </h2>
+      <div class="content">
+        <p>ipsum</p>
+      </div>
+
+      <!-- put the above into components ... -->
+    </div>
+  </section>
+</template>
