d1a77882f5359268f0772d81261383162c9df29a
UH <554c46@gmail.com>
2021-07-01 15:54:46 +0200

    settings header changed

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -6,22 +6,30 @@
   <section class="section" id="settings">
     <div class="container">
       <!-- put the following into components ... -->
-      <h1 class="title is-3 is-spaced">{{ t('settings.settings') }}</h1>
+      <h1 class="title is-3 is-spaced">{{ t('settings.general') }}</h1>
 
-      <h2 class="subtitle is-4">{{ t('settings.appearance') }}</h2>
+      <h2 class="subtitle is-5">{{ t('settings.appearance') }}</h2>
       <div class="field">
         <input id="darkmode-toogle" class="switch is-rounded" type="checkbox"   
                v-model="darkmodetheme">
         <label for="darkmode-toogle">Darkmode</label>
       </div>
 
-      <h2 class="subtitle is-4">{{ t('settings.language') }}</h2>
+      <h2 class="subtitle is-5">{{ t('settings.language') }}</h2>
       <div class="field">
         <LanguageSwitcher />
       </div>
 
 
-      <h2 class="subtitle is-4">Offline and Sync Settings</h2>
+      <h1 class="title is-3 is-spaced">BWS v3</h1>
+      <div class="content">
+        <p>
+          The following settings are only applicable for the Best-Worst Scaling UI v3.
+          The sampling occurs <b>inside the REST API</b>, when the app's inventory of example sets falls under a threshold.
+        </p>
+      </div>
+
+      <h2 class="subtitle is-5">Offline and Sync Settings</h2>
       <div class="columns">
       <div class="column is-narrow-tablet is-narrow-desktop is-narrow-widescreen is-narrow-fullhd">
     
@@ -49,7 +57,7 @@
       </div>
 
 
-      <h2 class="subtitle is-4">Sampling Settings</h2>
+      <h2 class="subtitle is-5">Sampling Settings</h2>
       <div class="columns">
       <div class="column is-narrow-tablet is-narrow-desktop is-narrow-widescreen is-narrow-fullhd">
     
@@ -75,6 +83,15 @@
 
       </div>
       </div>
+
+
+      <h1 class="title is-3 is-spaced">BWS v4</h1>
+      <div class="content">
+        <p>
+          The following settings are only applicable for the Best-Worst Scaling UI v4.
+        </p>
+      </div>
+
       <!-- put the above into components ... -->
     </div>
   </section>
