a1e599bc68f5b2808464da5a9f1ddc76038e4d41
UH <554c46@gmail.com>
2021-07-09 15:49:16 +0200

    common code moved to queue.js module

--- src/views/bestworst/BestWorst3.vue
+++ src/views/bestworst/BestWorst3.vue
@@ -38,11 +38,11 @@
 import TheNavbar from '@/components/layout/TheNavbar.vue';
 import PageLoader from '@/components/layout/PageLoader.vue';
 import BestWorstChoices from '@/components/bestworst/Choices.vue';
-import { defineComponent, reactive, watchEffect, watch, unref, ref, computed } from 'vue';
+import { defineComponent, watchEffect, watch, unref, computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useApi, useAuth } from '@/functions/axios-evidence.js';
 import { useSettings } from '@/functions/settings.js';
-import { traverseObject } from '@/functions/traverse-objects.js';
+import { useBwsQueue } from '@/components/bestworst/queue.js';
 
 
 export default defineComponent({
@@ -65,34 +65,23 @@ export default defineComponent({
 
     // Load bestworst3 UI settings
     const { 
-      loadSettings, reorderpoint, orderquantity,
+      loadSettings, 
+      reorderpoint, orderquantity,
       sampling_numtop, sampling_offset 
     } = useSettings();
+    loadSettings();
 
-    // Search string for lemmata/keywords
-    const searchlemmata = ref("");
+    // Load reactive variables for BWS Queue
+    const { 
+      uispec, searchlemmata, data, 
+      isReplenishing, isSaving, message_suggestion,
+      pullFromQueue, nextExampleSet, 
+      resetQueue
+    } = useBwsQueue();
 
-    // reactive data of this component
-    const data = reactive({
-      // Array with unlabelled example sets. It's a FIFO queue
-      queue: [],
+    // configure UI meta info
+    uispec["name"] = "bestworst3";
 
-      // The current BWS-exampleset displayed inside the app
-      current: [],
-      current_setid: undefined,
-      current_lemmata: undefined, //'Hello world,cool',
-
-      // Use to trigger component re-rendering with :key
-      counter: 1,
-
-      // Post this to the REST API (see saveEvaluations)
-      evaluated: [],
-    });
-
-    // AJAX status flags and messages
-    const isReplenishing = ref(false);
-    const isSaving = ref(false);
-    const message_suggestion = ref("Not connected! Please login.");
 
     // Replenish data.queue from database (load new example sets into queue)
     const replenishQueue = () => {
@@ -132,72 +121,20 @@ export default defineComponent({
         .finally(() => {
           isReplenishing.value = false;
           console.log(`Queue replenished to ${data.queue.length} examplesets`);
+          if (data.current.length === 0){
+            pullFromQueue(); // load data
+            console.log("New current BWS set loaded")
+          }
+          console.log("After API Call:", data)
         });
       });
     }
 
-    // Pull new current example set from queue
-    async function pullFromQueue(){
-      // Trigger initial replenishment if the data.queue is empty
-      if (data.queue.length == 0){
-        await loadSettings();  // wait till settings are loaded
-        await replenishQueue();  // wait till finished
-      }
-      // Read the 1st element, and delete it from queue (FIFO principle)
-      const tmp = data.queue.shift()
-      if (typeof tmp !== 'undefined' ){
-        //console.log(tmp)
-        data.current = tmp.examples;
-        data.current_setid = tmp.set_id;
-        data.current_lemmata = tmp.lemmata.join(", ");
-      }else{
-        data.current = [];
-        data.current_setid = undefined;
-        data.current_lemmata = undefined;
-      }
-    }
-
-    // Store evaluation results, pull next example set from queue, trigger re-rendering
-    async function nextExampleSet(history){
-      // abort if the setId still exists in data.evaluated, i.e. the current setId
-      // was not saved yet but the user keeps pushing the submit buttons (i.e. calling
-      // the `nextExampleSet` function)
-      data.evaluated.forEach(elem => {
-        if (elem['set-id'] === data.current_setid){
-          console.log(`The set-id='${elem['set-id']}' cannot be stored twice`);
-          return;
-        }
-      });
-      //console.log(data.evaluated)
-      // Map states with SentenceIDs (Don't send raw examples `data.current` back to API)
-      var state_sentid_map = {}
-      data.current.forEach((ex, i) => state_sentid_map[i] = ex.id)
-      // Store latest evaluation
-      data.evaluated.push({
-        'set-id': data.current_setid,  // Only required for App/API-Sync
-        'ui-name': 'bestworst3',
-        'lemmata': data.current_lemmata.split(',').map(s => s.trim()),
-        'event-history': JSON.parse(JSON.stringify(history)),  // to be stored in DB
-        'state-sentid-map': state_sentid_map,  // to be stored in DB
-        'tracking-data': {
-          'window': traverseObject(window, 0),
-          'screen': traverseObject(window.screen, 1),
-          'navigator': traverseObject(window.navigator, 1)
-        }
-      });
-      // Load the next example set
-      if (!isReplenishing.value){
-        await pullFromQueue();
-      }
-      // enforce rerendering via :key
-      data.counter++;
-    }
 
     // trigger AJAX request to replenish the queue
     watch(
       () => data.queue.length,
       (stocklevel) => {
-        //const reorderpoint = 3;
         if (stocklevel < reorderpoint.value){
           console.log(`Queue is running low: ${stocklevel} examplesets`);
           replenishQueue();
@@ -247,32 +184,29 @@ export default defineComponent({
     );
 
 
-    // get search field string to parent component
+    /**
+     * Store the new Lemma, Reset the Queue data, Load new data
+     */
     const onSearchLemmata = async(keywords) => {
+      // delete current example set in UI, and the whole queue.
+      resetQueue();
       // reset `searchlemmata`
       searchlemmata.value = keywords
-      //console.log('Bestworst:', searchlemmata.value)
-      
-      // delete current example set in UI
-      data.current = [];
-      data.current_setid = undefined;
-      data.current_lemmata = undefined;
-      // this will trigger the watcher to call `replenishQueue` (POST requests)
-      data.queue = [];  
       // force to load next example in UI
-      await pullFromQueue();
+      await replenishQueue();
     }
 
 
     // load initial current BWS-exampleset
-    pullFromQueue();
+    replenishQueue();
+
 
     // compute max for progressba
     const maxprogress = computed(() => parseInt(reorderpoint.value) + parseInt(orderquantity.value));
 
+
     return { 
       data, 
-      pullFromQueue,  // <= is this needed?
       nextExampleSet,
       maxprogress,
       onSearchLemmata,
