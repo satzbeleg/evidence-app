0e975f026940cd189b2dba64badf350d37d7c404
UH <554c46@gmail.com>
2022-01-19 12:08:19 +0100

    histpdf func moved into seperate npm pkg

--- src/components/bestworst/interactivity.js
+++ src/components/bestworst/interactivity.js
@@ -1,5 +1,6 @@
 import { reactive, ref, watch, unref } from 'vue';
 import { sampling, counting, ranking } from 'bwsample';
+import { histpdf } from 'histpdf';
 import { useApi2, useAuth } from '@/functions/axios-evidence.js';
 // import { v4 as uuid4 } from 'uuid'; // nur für dev
 import { useBwsSettings } from '@/components/bestworst/bws-settings.js';
@@ -30,47 +31,6 @@ const getLastAbsChange = (arr) => {
   }else{
     return undefined
   }
-}
-
-
-/**
- * Computes the Density (PDF) for a given histogram bin
- * 
- * Example:
- * --------
- *    const data = Array.from({length: 40}, () => Math.random())
- *    const bin_edges = [.1, .25, .5, .75, 9.].sort()
- *    const probas = histpdf(data, bin_edges)
- */
-const histpdf = (data, bin_edges) => {
-  const num_bins = bin_edges.length - 1;
-  var count = Array.from({length: num_bins}, () => 0);
-  var i;
-  // Count occurences inside the bins
-  data.forEach(val => {
-    if (val <= bin_edges[0]){
-      count[0] += 1;
-      return;  // break "forEach" loop
-    }
-    if (val > bin_edges[num_bins]){
-      count[0] += 1;
-      return;  // break "forEach" loop
-    }
-    for(i = 0; i < num_bins; i++){
-      if (bin_edges[i] < val && val <= bin_edges[i+1]){
-        count[i] += 1;
-        continue;  // break "for" loop
-      }
-    }
-  })
-  // convert counts to probabilities
-  const num_sample_size = data.length;
-  var probas = []
-  count.forEach(val => {
-    probas.push(val / num_sample_size);
-  })
-  // done
-  return probas;
 }
 
 
