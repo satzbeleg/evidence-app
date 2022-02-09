e4d4d088dfc6054fe55c70e8e3aaf1910fdca4dd
UH <554c46@gmail.com>
2022-02-09 17:54:20 +0100

    fix#218

--- src/translations/pages/tips-de.vue
+++ src/translations/pages/tips-de.vue
@@ -3,9 +3,15 @@
   <p>
   </p>
 
-  <!-- <h2 class="subtitle is-5">Lorem Ipsum</h2>
+  <h2 class="subtitle is-5">Videoanleitungen</h2>
   <div class="content">
-  </div> -->
+    <p>
+      Hinweise zur Bedienung können sich hier angeschaut werden:
+      <a href="https://youtube.com/playlist?list=PL5ZUTHo0MPd6GvUm2oiS8pBzhYZFloZhn" target="blank_">Youtube Playliste</a>
+    </p>
+
+    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/videoseries?list=PL5ZUTHo0MPd6GvUm2oiS8pBzhYZFloZhn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
 
 </template>
 
