697052ed56108411fa0679c94f11b55b2421a4a9
UH <554c46@gmail.com>
2022-02-09 12:17:32 +0100

    components for long text translations

--- src/views/legal/Terms.vue
+++ src/views/legal/Terms.vue
@@ -4,17 +4,8 @@
              v-bind:with_lemmata_search="false" />
   <section class="section" id="terms">
     <div class="container">
-      <!-- put the following into components ... -->
-      
-      <h1 class="title is-3">{{ t('terms.title') }}</h1>
-      <p></p>
-
-      <h2 class="subtitle is-5">Lorem </h2>
-      <div class="content">
-        <p>ipsum</p>
-      </div>
-
-      <!-- put the above into components ... -->
+      <div v-show="locale === 'de'"><TermsDE/></div>
+        
     </div>
   </section>
 </template>
@@ -24,23 +15,26 @@
 import TheNavbar from '@/components/layout/TheNavbar.vue';
 import { useI18n } from 'vue-i18n';
 import { watchEffect } from "vue";
-
+import TermsDE from '@/translations/terms-de.vue';
 
 export default {
   name: "Terms of Use",
 
   components: {
-    TheNavbar
+    TheNavbar,
+    TermsDE
   },
 
   setup(){
-    const { t } = useI18n();
+    const { t, locale } = useI18n();
 
     watchEffect(() => {
       document.title = t('terms.title');
     });
 
-    return { t }
+    console.log("Sprache: ", locale.value)
+
+    return { t, locale }
   }
 }
 </script>
