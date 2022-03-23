27d7435eca11af59f4e3629222a904cb7a79caed
Ulf <554c46@gmail.com>
2022-03-23 21:44:10 +0100

    HTML boxes and buttons

--- src/views/diversity/Diversity1.vue
+++ src/views/diversity/Diversity1.vue
@@ -10,10 +10,26 @@
       <h1 class="title is-3">Find diverse sets of sentence examples</h1>
       <p></p>
 
-      <button class="button" v-on:click="showEditModal = true">
-        <i class="fas fa-cog" />
-        Filter Search
-      </button>
+      <div class="field is-grouped is-grouped-centered"> 
+        <p class="control">
+          <button class="button is-rounded is-info" 
+                  v-on:click="showEditModal = true">
+            <span class="icon"><i class="fas fa-filter"></i></span>
+            <strong>Filter Settings</strong>
+          </button>
+        </p>
+      </div>
+
+      <div class="card is-quarter" v-for="(item, idx) in sentenceExamples" :key="idx">
+        <div class="card-content">
+          <div class="content center">
+            <!-- <div v-fit2box="item.text" class="fixed-box"> -->
+              <div v-html="highlightSpans(item.text, item.spans, 'span', 'tag is-success is-light is-rounded reset-to-parent-font-height')"></div>
+            <!-- </div> -->
+          </div>
+        </div>
+      </div>
+
 
       <!-- put the above into components ... -->
     </div>
@@ -28,70 +44,77 @@
       </header>
       <section class="modal-card-body">
         <!-- Content ... -->
+        <div class="columns">
+          <div class="column is-narrow-tablet is-narrow-desktop is-narrow-widescreen is-narrow-fullhd">
+          
+            <div class="field">
+              <label class="label" for="item-goodness-score">
+                Goodness Score (0.0 - 1.0)
+              </label>
+              <input id="item-goodness-score" 
+                    class="slider has-output is-fullwidth is-primary is-circle is-medium" 
+                    type="range" v-model="goodnessScore" step="0.01" min="0" max="1">
+              <output for="item-sampling-numtop" style="width:3.1rem;">{{ goodnessScore }}</output>
+            </div>
 
-  <div class="columns">
-    <div class="column is-narrow-tablet is-narrow-desktop is-narrow-widescreen is-narrow-fullhd">
-    
-      <div class="field">
-        <label class="label" for="item-goodness-score">
-          Goodness Score (0.0 - 1.0)
-        </label>
-        <input id="item-goodness-score" 
-               class="slider has-output is-fullwidth is-primary is-circle is-medium" 
-               type="range" v-model="goodnessScore" step="0.01" min="0" max="1">
-        <output for="item-sampling-numtop" style="width:3.1rem;">{{ goodnessScore }}</output>
-      </div>
+            <div class="field">
+              <label class="label" for="item-diversity-semantic">
+                Semantic
+              </label>
+              <input id="item-diversity-score" 
+                    class="slider has-output is-fullwidth is-primary is-circle is-medium" 
+                    type="range" v-model="diversitySemantic" step="0.01" min="0" max="1">
+              <output for="item-sampling-numtop" style="width:3.1rem;">{{ diversitySemantic }}</output>
+            </div>
 
-      <div class="field">
-        <label class="label" for="item-diversity-semantic">
-          Semantic
-        </label>
-        <input id="item-diversity-score" 
-               class="slider has-output is-fullwidth is-primary is-circle is-medium" 
-               type="range" v-model="diversitySemantic" step="0.01" min="0" max="1">
-        <output for="item-sampling-numtop" style="width:3.1rem;">{{ diversitySemantic }}</output>
-      </div>
+            <div class="field">
+              <label class="label" for="item-diversity-syntax">
+                Syntax
+              </label>
+              <input id="item-diversity-syntax" 
+                    class="slider has-output is-fullwidth is-primary is-circle is-medium" 
+                    type="range" v-model="diversitySyntax" step="0.01" min="0" max="1">
+              <output for="item-sampling-numtop" style="width:3.1rem;">{{ diversitySyntax }}</output>
+            </div>
 
-      <div class="field">
-        <label class="label" for="item-diversity-syntax">
-          Syntax
-        </label>
-        <input id="item-diversity-syntax" 
-               class="slider has-output is-fullwidth is-primary is-circle is-medium" 
-               type="range" v-model="diversitySyntax" step="0.01" min="0" max="1">
-        <output for="item-sampling-numtop" style="width:3.1rem;">{{ diversitySyntax }}</output>
-      </div>
+            <div class="field">
+              <label class="label" for="item-diversity-fingerprint">
+                Fingerprint
+              </label>
+              <input id="item-diversity-fingerprint" 
+                    class="slider has-output is-fullwidth is-primary is-circle is-medium" 
+                    type="range" v-model="diversityFingerprint" step="0.01" min="0" max="1">
+              <output for="item-sampling-numtop" style="width:3.1rem;">{{ diversityFingerprint }}</output>
+            </div>
 
-      <div class="field">
-        <label class="label" for="item-diversity-fingerprint">
-          Fingerprint
-        </label>
-        <input id="item-diversity-fingerprint" 
-               class="slider has-output is-fullwidth is-primary is-circle is-medium" 
-               type="range" v-model="diversityFingerprint" step="0.01" min="0" max="1">
-        <output for="item-sampling-numtop" style="width:3.1rem;">{{ diversityFingerprint }}</output>
-      </div>
-
-      <div class="field">
-        <label class="label" for="item-diversity-meta">
-          Meta information
-        </label>
-        <input id="item-diversity-meta" 
-               class="slider has-output is-fullwidth is-primary is-circle is-medium" 
-               type="range" v-model="diversityMeta" step="0.01" min="0" max="1">
-        <output for="item-sampling-numtop" style="width:3.1rem;">{{ diversityMeta }}</output>
-      </div>
-
-
-    </div>
-  </div>
-
-
+            <div class="field">
+              <label class="label" for="item-diversity-meta">
+                Meta information
+              </label>
+              <input id="item-diversity-meta" 
+                    class="slider has-output is-fullwidth is-primary is-circle is-medium" 
+                    type="range" v-model="diversityMeta" step="0.01" min="0" max="1">
+              <output for="item-sampling-numtop" style="width:3.1rem;">{{ diversityMeta }}</output>
+            </div>
+          </div>
+        </div>
       </section>
       <footer class="modal-card-foot">
-        <button class="button is-success" v-on:click="showEditModal = false">Recompute</button>
-        <button class="button" v-on:click="showEditModal = false">Reset</button>
-        <button class="button" v-on:click="showEditModal = false">Cancel</button>
+        <button class="button is-rounded is-danger is-light" 
+                v-on:click="showEditModal = false">
+          <span class="icon"><i class="fas fa-trash"></i></span>
+          <strong>Reset</strong>
+        </button>
+        <button class="button is-rounded is-warning is-light" 
+                v-on:click="showEditModal = false">
+          <!-- <span class="icon"><i class="fas fa-filter" /></span> -->
+          <strong>Cancel</strong>
+        </button>
+        <button class="button is-rounded is-success" 
+                v-on:click="showEditModal = false">
+          <span class="icon"><i class="fas fa-repeat"></i></span>
+          <strong>Recompute</strong>
+        </button>
       </footer>
     </div>
   </div>
@@ -102,10 +125,11 @@
 <script>
 import TheNavbar from '@/components/layout/TheNavbar.vue';
 import { useI18n } from 'vue-i18n';
-import { watchEffect, ref } from "vue";
+import { watchEffect, ref, reactive } from "vue";
+import { highlightSpans } from '@/functions/highlight-spans.js';
 
 export default {
-  name: "Terms of Use",
+  name: "Find diverse sets of sentence examples",
 
   components: {
     TheNavbar
@@ -122,8 +146,13 @@ export default {
     const diversityFingerprint = ref(0.0);
     const diversityMeta = ref(0.0);
 
+    const sentenceExamples = reactive([
+      {"id": "123", "text": "Das Haus ist groß.", "spans": [[4, 8]]},
+      {"id": "123", "text": "Das Haus war blau, aber nun gelb bevor es angemalt wurde.", "spans": [[4, 8]]},
+    ]);
+
     watchEffect(() => {
-      document.title = t('terms.title');
+      document.title = t('general.title');
     });
 
     return { 
@@ -134,7 +163,15 @@ export default {
       diversitySyntax,
       diversityFingerprint,
       diversityMeta,
+      sentenceExamples,
+      highlightSpans
     }
   }
 }
 </script>
+
+<style scoped>
+.card {
+  margin-bottom: min(2vh, 25px); /* min(2vh, 25px); */
+}
+</style>
