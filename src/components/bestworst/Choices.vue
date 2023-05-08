4dceccf80c8fdf30b26c4c25f204194a7857742b
Ulf Hamster <554c46@gmail.com>
2023-05-08 10:10:58 +0200

    old code deleted

--- src/components/bestworst/Choices.vue
+++ src/components/bestworst/Choices.vue
@@ -9,42 +9,6 @@
       v-on:item-selected="onTransition"
     />
 
-    <!-- <ItemCard 
-      itemPos="0"
-      v-bind:itemState="data.states[0]"
-      v-bind:sentId="items[0].id"
-      v-bind:sentText="items[0].text"
-      v-bind:lemmaSpans="items[0].spans"
-      v-on:item-selected="onTransition"
-    />
-
-    <ItemCard 
-      itemPos="1"
-      v-bind:itemState="data.states[1]"
-      v-bind:sentId="items[1].id"
-      v-bind:sentText="items[1].text"
-      v-bind:lemmaSpans="items[1].spans"
-      v-on:item-selected="onTransition"
-    />
-
-    <ItemCard 
-      itemPos="2"
-      v-bind:itemState="data.states[2]"
-      v-bind:sentId="items[2].id"
-      v-bind:sentText="items[2].text"
-      v-bind:lemmaSpans="items[2].spans"
-      v-on:item-selected="onTransition"
-    />
-
-    <ItemCard 
-      itemPos="3"
-      v-bind:itemState="data.states[3]"
-      v-bind:sentId="items[3].id"
-      v-bind:sentText="items[3].text"
-      v-bind:lemmaSpans="items[3].spans"
-      v-on:item-selected="onTransition"
-    /> -->
-    
     <div class="field is-grouped is-grouped-centered">  <!-- style="max-width: 400px;" -->
       <!-- Skip -->
       <p class="control">
