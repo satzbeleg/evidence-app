7060b8748ea5f4f16c3c2594161052dff1a38b86
Ulf Hamster <554c46@gmail.com>
2023-05-23 18:50:53 +0200

    fix #264 compute similarities in the browser

---
+++ src/views/variation/Variation2.vue
@@ -0,0 +1,419 @@
+<template>
+  <TheNavbar v-bind:with_lang_switch="false"
+             v-bind:with_darkmode_icon="false"
+             v-bind:with_headword_search="true"
+             v-bind:search_string="search_headword"
+             v-on:search-headword-navbar="onSearchHeadword"
+             :key="search_headword" />
+
+  <PageLoader v-if="isLoading"
+    v-bind:status="isLoading"
+    v-bind:messages="['Loading Data', 'Computing similarities', 'Solve optimization problem']" />
+
+  <section class="section" id="variation" v-else>
+    <div class="container">
+      <!-- put the following into components ... -->
+      
+      <!-- <h1 class="title is-3">Find diverse sets of sentence examples</h1> -->
+      <!-- <p></p> -->
+
+      <div 
+        class="field is-grouped is-grouped-centered" 
+        style="position: sticky; display: inline-block;"
+        v-if="pool"
+      > 
+        <p class="control">
+          <button class="button is-rounded is-info" 
+                  v-on:click="showEditModal = true">
+            <span class="icon"><i class="fas fa-filter"></i></span>
+            <strong>Preference Parameters</strong>
+          </button>
+        </p>
+      </div>
+
+      <div class="card is-quarter" 
+        v-for="(item, idx) in sortedPool" 
+        :key="idx"
+      >
+        <div class="card-content">
+          <div v-html="highlightSpans(item.text, item.spans, 'span', 'tag is-success is-light is-rounded reset-to-parent-font-height')"></div>
+          <p class="is-size-7 has-text-grey is-italic">
+            w<sub>{{ item.id }}</sub>: {{ parseFloat(item.weight).toFixed(8) }} |
+            {{ item.bibl }}
+          </p>
+        </div>
+      </div>
+
+      <!-- put the above into components ... -->
+    </div>
+  </section>
+
+  <div class="modal" :class="{ 'is-active': showEditModal }">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Search Settings</p>
+        <button class="delete" aria-label="close" v-on:click="showEditModal = false"></button>
+      </header>
+      <section class="modal-card-body">
+        <!-- Content ... -->
+        <div class="columns">
+          <div class="column is-narrow-tablet is-narrow-desktop is-narrow-widescreen is-narrow-fullhd">
+          
+            <div class="field">
+              <label class="label" for="item-goodness-score">
+                Variation (0) vs Goodness Score (100)
+              </label>
+              <input id="item-goodness-score" 
+                    class="slider has-output is-fullwidth is-primary is-circle is-medium" 
+                    type="range" v-model="lambdaTradeOff" step="0.01" min="0.01" max="0.99">
+              <output for="item-goodness-score" style="width:3.1rem;">{{ lambdaTradeOff * 100 }}</output>
+            </div>
+
+            <div class="field">
+              <label class="label" for="item-beta-semantic">
+                Semantic
+              </label>
+              <input id="item-beta-semantic" 
+                    class="slider has-output is-fullwidth is-primary is-circle is-medium" 
+                    type="range" v-model="betaSemantic" step="0.01" min="0.0" max="1.0">
+              <output for="item-beta-semantic" style="width:3.1rem;">{{ betaSemantic * 100 }}</output>
+            </div>
+
+            <div class="field">
+              <label class="label" for="item-beta-grammar">
+                Grammar
+              </label>
+              <input id="item-beta-grammar" 
+                    class="slider has-output is-fullwidth is-primary is-circle is-medium" 
+                    type="range" v-model="betaGrammar" step="0.01" min="0.0" max="1.0">
+              <output for="item-beta-grammar" style="width:3.1rem;">{{ betaGrammar * 100 }}</output>
+            </div>
+
+            <div class="field">
+              <label class="label" for="item-beta-duplicate">
+                Near Duplicates
+              </label>
+              <input id="item-beta-duplicate" 
+                    class="slider has-output is-fullwidth is-primary is-circle is-medium" 
+                    type="range" v-model="betaDuplicate" step="0.01" min="0.0" max="1.0">
+              <output for="item-beta-duplicate" style="width:3.1rem;">{{ betaDuplicate * 100 }}</output>
+            </div>
+
+            <div class="field">
+              <label class="label" for="item-beta-biblio">
+                Bibliographic
+              </label>
+              <input id="item-beta-biblio" 
+                    class="slider has-output is-fullwidth is-primary is-circle is-medium" 
+                    type="range" v-model="betaBiblio" step="0.01" min="0.0" max="1.0">
+              <output for="item-beta-biblio" style="width:3.1rem;">{{ betaBiblio * 100 }}</output>
+            </div>
+
+            <!-- <div class="field">
+              <label class="label" for="item-num-examples">
+                Number of Examples
+              </label>
+              <input id="item-num-examples" 
+                    class="slider has-output is-fullwidth is-secondary is-circle is-medium" 
+                    type="range" v-model="numExamples" step="5" min="20" max="200">
+              <output for="item-num-examples" style="width:3.1rem;">{{ numExamples }}</output>
+            </div> -->
+          </div>
+        </div>
+      </section>
+
+      <footer class="modal-card-foot">
+        <button class="button is-rounded is-danger is-light" 
+                v-on:click="() => {lambdaTradeOff = .25; betaSemantic = .5; betaGrammar = 0.; betaDuplicate = 0.; betaBiblio = 0.;}">
+          <span class="icon"><i class="fas fa-trash"></i></span>
+          <strong>Reset</strong>
+        </button>
+        <button class="button is-rounded is-warning is-light" 
+                v-on:click="showEditModal = false">
+          <!-- <span class="icon"><i class="fas fa-filter" /></span> -->
+          <strong>Cancel</strong>
+        </button>
+        <button class="button is-rounded is-success" 
+                v-on:click="() => {showEditModal = false; recomputeMatrix = true;}">
+          <span class="icon"><i class="fas fa-repeat"></i></span>
+          <strong>Recompute</strong>
+        </button>
+      </footer>
+    </div>
+  </div>
+
+</template>
+
+
+<script>
+import TheNavbar from '@/components/layout/TheNavbar.vue';
+import PageLoader from '@/components/layout/PageLoader.vue';
+import { useI18n } from 'vue-i18n';
+import { 
+  watchEffect, 
+  ref, 
+  watch,
+  // getCurrentInstance 
+} from "vue";
+import { highlightSpans } from '@/functions/highlight-spans.js';
+import { useInteractivity } from '@/components/bestworst/interactivity.js';
+import { useQueue } from '@/components/bestworst/queue.js';
+import { useQuadOpt } from '@/components/variation/quadopt.js';
+// import ItemCard from '@/components/bestworst/ItemCard.vue';
+
+export default {
+  name: "Find diverse sets of sentence examples",
+
+  components: {
+    TheNavbar,
+    PageLoader,
+    // ItemCard
+  },
+
+  setup(){
+    const { t } = useI18n();
+
+    watchEffect(() => {
+      document.title = t('variation.title');
+    });
+
+    const showEditModal = ref(false);
+    const recomputeMatrix = ref(false);
+    const isLoading = ref(false);
+
+    const lambdaTradeOff = ref(.25);
+    const betaSemantic = ref(.5);
+    const betaGrammar = ref(.0);
+    const betaDuplicate = ref(.0);
+    const betaBiblio = ref(.0);
+
+    // Load Interactivity Settings
+    const { 
+      pool,  // All examples
+      resetPool,  // Only call on new headword
+      addExamplesToPool,  // load new examples from API/CQL
+      predictScores,  // Only call on new headword
+    } = useInteractivity();
+
+
+    // Global Current Headword
+    const { search_headword } = useQueue();
+
+    /* Load if new headword was submitted */
+    const onSearchHeadword = async(keywords) => {
+      // delete pool and pairs matrix
+      resetPool();
+      // reset `search_headword`
+      search_headword.value = keywords
+      // load new examples from API/CQL
+      await addExamplesToPool(search_headword.value);
+      // predict scores
+      await predictScores();
+      // compute all similarity pairs and its scores {id1: {id2: score}}
+      await computeSimilaries();
+      // solve optimization problem initiallly
+      updateWeights();
+      // console.log("pool", pool)
+    }
+    
+
+    /** Compute Similarity Matrices */
+    const hamming_distance = (hash1, hash2) => {
+      let distance = 0;
+      for (let i = 0; i < hash1.length; i++) {
+        if (hash1[i] !== hash2[i]) {
+          distance++;
+        }
+      }
+      return distance / hash1.length;
+    }
+
+    const computeSimilaries = async () => {
+      // init storage
+      Object.keys(pool).forEach((key) => {
+        if(pool[key]['similarities'] === undefined) {
+          pool[key]['similarities'] = {
+            "semantic": {}, "grammar": {}, "duplicate": {}, "biblio": {}}
+        }
+      });
+      // compute similarities if not exits
+      Object.keys(pool).forEach((key1) => {
+        Object.keys(pool).forEach((key2) => {
+          if(key1 !== key2){
+            if(pool[key1]['similarities']['semantic'][key2] === undefined) {
+              pool[key1]['similarities']['semantic'][key2] = hamming_distance(
+                pool[key1]['hashes']['semantic'], pool[key2]['hashes']['semantic']);
+                pool[key2]['similarities']['semantic'][key1] = pool[key1]['similarities']['semantic'][key2] 
+            }
+            if(pool[key1]['similarities']['grammar'][key2] === undefined) {
+              pool[key1]['similarities']['grammar'][key2] = hamming_distance(
+                pool[key1]['hashes']['grammar'], pool[key2]['hashes']['grammar']);
+                pool[key2]['similarities']['grammar'][key1] = pool[key1]['similarities']['grammar'][key2] 
+            }
+            if(pool[key1]['similarities']['duplicate'][key2] === undefined) {
+              pool[key1]['similarities']['duplicate'][key2] = hamming_distance(
+                pool[key1]['hashes']['duplicate'], pool[key2]['hashes']['duplicate']);
+                pool[key2]['similarities']['duplicate'][key1] = pool[key1]['similarities']['duplicate'][key2] 
+            }
+            if(pool[key1]['similarities']['biblio'][key2] === undefined) {
+              pool[key1]['similarities']['biblio'][key2] = hamming_distance(
+                pool[key1]['hashes']['biblio'], pool[key2]['hashes']['biblio']);
+                pool[key2]['similarities']['biblio'][key1] = pool[key1]['similarities']['biblio'][key2] 
+            }
+          }
+        });
+      });
+    }
+
+
+
+
+    /* Sort by weights */
+    // const sortByWeight = (arr_) => {
+    //   let arr = Object.values(arr_)
+    //   if (arr.length > 0){
+    //     console.log("arr", arr)
+    //     return arr.slice().sort((a, b) => {return b.weight - a.weight})
+    //   }
+    //   return arr
+    // }
+
+
+
+    /** Aggregate matrices if ... 
+     * - new sentences in `sentenceExamples`
+     * - preference params changed, e.g. `betaSemantic`
+     */ 
+    const { 
+      aggregate_matrices, 
+      get_weights 
+    } = useQuadOpt()
+
+
+    // Specify matrices
+    watch(
+      () => recomputeMatrix.value,
+      (flag) => {
+        if (flag){
+          updateWeights();
+          recomputeMatrix.value = false;
+        }
+      }
+    );
+
+    const sortedPool = ref([]);
+    const sortPool = (obj) => {
+      let arr = Object.values(obj) 
+      if (arr.length > 0){
+        sortedPool.value = arr.slice().sort((a, b) => {return b.weight - a.weight})
+      }
+    }
+
+    const updateWeights = () => {
+      // open pagerloader
+      isLoading.value = true;
+      console.group();
+      console.log("Lambda:", parseFloat(lambdaTradeOff.value));
+      console.log("Semantic:", parseFloat(betaSemantic.value));
+      console.log("Grammar:", parseFloat(betaGrammar.value));
+      console.log("Duplicates:", parseFloat(betaDuplicate.value));
+      console.log("Bibliographic:", parseFloat(betaBiblio.value));
+      console.groupEnd();
+
+      // extract data from pool
+      let numSamples = Object.keys(pool).length;
+      let good_scores = new Array(numSamples);
+      let simi_semantic = new Array(numSamples);
+      let simi_grammar = new Array(numSamples);
+      let simi_duplicate = new Array(numSamples);
+      let simi_biblio = new Array(numSamples);
+      let i = 0;
+      let j = 0;
+      // let tmp = Object.values(pool); console.log("tmp", tmp)
+      let orderedKeys = new Array(numSamples);
+      Object.keys(pool).forEach((key1) => {
+        orderedKeys[i] = key1;
+        good_scores[i] = pool[key1]['last_model_score'] || 0.5;
+        simi_semantic[i] = new Array(numSamples);
+        simi_grammar[i] = new Array(numSamples);
+        simi_duplicate[i] = new Array(numSamples);
+        simi_biblio[i] = new Array(numSamples);
+        j = 0;
+        Object.keys(pool).forEach((key2) => {
+          if(key1 === key2){
+            simi_semantic[i][j] = 1.0;
+            simi_grammar[i][j] = 1.0;
+            simi_duplicate[i][j] = 1.0;
+            simi_biblio[i][j] = 1.0;
+          } else {     
+            simi_semantic[i][j] = pool[key1]['similarities']['semantic'][key2];
+            simi_grammar[i][j] = pool[key1]['similarities']['grammar'][key2];
+            simi_duplicate[i][j] = pool[key1]['similarities']['duplicate'][key2];
+            simi_biblio[i][j] = pool[key1]['similarities']['biblio'][key2];
+          }
+          j++;
+        });
+        i++;
+      });
+      // console.log("Good scores:", good_scores);
+      // console.log("Semantic:", simi_semantic);
+      // console.log("Grammar:", simi_grammar);
+      // console.log("Duplicates:", simi_duplicate);
+      // console.log("Bibliographic:", simi_biblio);
+
+      // // aggregate similarity matrices
+      let simi = aggregate_matrices(
+        simi_semantic, parseFloat(betaSemantic.value), 
+        simi_grammar, parseFloat(betaGrammar.value),
+        simi_duplicate, parseFloat(betaDuplicate.value),
+        simi_biblio, parseFloat(betaBiblio.value)
+      );
+      simi.print();
+      // solve problem
+      let wbest = get_weights(
+        good_scores, simi, parseFloat(lambdaTradeOff.value), undefined, 25);
+
+      // assign weights
+      let arr = wbest.arraySync();
+      console.log("Weights:", arr);
+      let k = 0;
+      orderedKeys.forEach((key) => {
+        pool[key].weight = arr[k];
+        k++;
+      });
+      sortPool(pool);
+
+      // // force rerendering
+      // console.log("Force rerendering")
+      // const instance = getCurrentInstance();
+      // instance?.proxy?.$forceUpdate();
+
+      // close pagerloader
+      isLoading.value = false;
+    }
+    
+
+
+    return { 
+      t,
+      search_headword,
+      onSearchHeadword,
+      isLoading,
+      showEditModal,
+      recomputeMatrix,
+      lambdaTradeOff,
+      betaSemantic, betaGrammar, betaDuplicate, betaBiblio,
+      pool, sortedPool, //sortByWeight,
+      // renderCards,
+      highlightSpans
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  margin-bottom: min(2vh, 25px); /* min(2vh, 25px); */
+}
+</style>
