3d51ab2359fbbca8bdcdd71b55e293b9227678a8
Ulf Hamster <554c46@gmail.com>
2023-06-20 15:18:52 +0200

    simplify settings

--- src/components/bestworst/ModalBwsSearchSettings.vue
+++ src/components/bestworst/ModalBwsSearchSettings.vue
@@ -14,41 +14,35 @@
           v-on:search-headword-field="onSearchHeadword" />
 
         <!-- Copied from BwsSettings.vue -->
-        <h2 class="subtitle is-4">Pool Size (v4)</h2>
-          <div class="content">
-            <p v-if="language == 'de'">
-              Die Poolgröße der lokal gespeicherten Satzbelege, Merkmalsvektoren, usw. muss eingeschränkt werden, da Endbenutzergeräte begrenzte Speicherkapazitäten haben.
-            </p>
-            <p v-else>
-              The pool size of the locally stored sentences, feature vectors, annotation data, etc. must be constrained as end user devices have limited storage capacities.
-            </p>
-          </div>
-          <div class="columns">
-            <div class="column is-narrow-tablet is-narrow-desktop is-narrow-widescreen is-narrow-fullhd">
+        <h2 class="subtitle is-4">Pool Size</h2>
+        <div class="content">
+          <template v-if="['de', 'de-leicht'].includes(language)">
+            Die maximale Anzahl der Satzbeispiele, die in den Cache Ihres Geräts geladen werden.
+              (Warnung: Wenn Sie 'semantisch-similar' ausgewählt haben, kann es beim Laden der Daten zu einer spürbaren Verzögerung kommen.)
+          </template>
+          <template v-else>
+            The maximum number of sentence examples that are loaded into the cache in your device.
+            (Warning: If you selected 'semantic-similar', a noticeable delay may occur while loading the data.)
+          </template>
+        </div>
+        <div class="columns">
+          <div class="column is-narrow-tablet is-narrow-desktop is-narrow-widescreen is-narrow-fullhd">
 
-              <div class="field">
-                <label class="label" for="min-pool-size">
-                  Minimum Pool Size
-                </label>
-                <input id="min-pool-size" 
-                      class="slider has-output is-fullwidth is-primary is-circle is-medium" 
-                      type="range" v-model="min_pool_size" step="10" min="10" max="1000">
-                <output for="min-pool-size">{{ min_pool_size }}</output>
-              </div>
-
-              <div class="field">
-                <label class="label" for="interactivity-max_pool_size">
-                  Maximum Pool Size
-                </label>
-                <input id="interactivity-max_pool_size" 
-                      class="slider has-output is-fullwidth is-primary is-circle is-medium" 
-                      type="range" v-model="max_pool_size" step="10" min="20" max="1000">
-                <output for="interactivity-max_pool_size">{{ max_pool_size }}</output>
-              </div>
-
+            <div class="field">
+              <label class="label" for="interactivity-max_pool_size">
+                Maximum Pool Size
+              </label>
+              <input id="interactivity-max_pool_size" 
+                    class="slider has-output is-fullwidth is-primary is-circle is-medium" 
+                    type="range" v-model="max_pool_size" step="10" min="20" max="1000">
+              <output for="interactivity-max_pool_size">{{ max_pool_size }}</output>
             </div>
+
           </div>
-          <!-- Content ... -->
+        </div>
+
+        
+        <!-- Content ... -->
 
         
 
@@ -63,6 +57,7 @@ import { useI18n } from 'vue-i18n';
 import { defineComponent, watch,  } from "vue";
 import HeadwordSearch from "@/components/layout/HeadwordSearch.vue";
 import { useBwsSettings } from '@/components/bestworst/bws-settings.js';
+import { useGeneralSettings } from '@/components/settings/general-settings.js';
 
 
 export default defineComponent({
@@ -86,6 +81,7 @@ export default defineComponent({
 
   setup(props, {emit}){
     const { t } = useI18n();
+    const { language } = useGeneralSettings();
 
     // Copied from BwsSettings.vue
     const {
@@ -108,6 +104,7 @@ export default defineComponent({
 
     return { 
       t,
+      language,
       min_pool_size,
       max_pool_size,
       onSearchHeadword
